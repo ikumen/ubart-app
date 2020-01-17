e5f5c48ddfd83d799b9fa24320b1e5327c00b393
ikumen <ikumen@gnoht.com>
2020-01-16 17:28:37 -0800

    Additional patterns to route to index

--- server/web.py
+++ server/web.py
@@ -1,11 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, make_response
 
 
 bp = Blueprint('webapp', __name__, url_prefix='/')
 
 
+@bp.route('/add')
 @bp.route('/')
 def index():
     """Handles all routes to the single page web app."""
-    return render_template('index.html')
+    resp = make_response(render_template('index.html'))
+    resp.headers.set('Access-Control-Allow-Origin', '*')
+    return resp
 
